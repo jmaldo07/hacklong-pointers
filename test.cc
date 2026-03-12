a8e8b45784f473ccd9e9b85a045fc622c6d7b9b6
Jonathan Maldonado <jmaldo07@csu.fullerton.edu>
2026-03-12 18:49:33 +0000

    Finished hacklong, learned about how * and & can be used in the same way and the differences in active code

--- test.cc
+++ test.cc
@@ -1,1 +1,23 @@
+// Name: Jonathan Maldonado
+// id: 880640412
+#include <iostream>
+#include <string>
+#include <cstdint>
 
+void double_it (int& x) {
+    x *= 2;
+    std::cout << "x is " << x << "\n";
+}
+
+void double_it_pointer (int* x) {
+    std::uintptr_t y = reinterpret_cast<std::uintptr_t>(x);
+    *x *= 2;
+    std::cout << "x is " << *x << "\n";
+}
+
+int main () {
+    int value[] {5, 7, 13, 19, 23};
+    std::cout << "value is " << value[0] << "\n";\
+    double_it_pointer(value);
+    std::cout << "value is " << value[0] << "\n";
+}
